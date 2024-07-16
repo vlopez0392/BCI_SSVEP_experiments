45fcc297e26a088a6d648c9fdbe246142f2e15b9
Victor D. Lopez <vlopez0392@gmail.com>
2024-07-17 05:57:38 +0800

    Refactored eeg data analyzer andutilities

--- eeg_data_analyzer.py
+++ eeg_data_analyzer.py
@@ -2,50 +2,103 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-from mne import create_info, Annotations
 from mne.io import RawArray
+import utilities.utilities as utils
+
 
 ###Global variables 
 paradigm_onset_delay = 6.0 ##Initial Text and Fixation cross 
 eeg_filepath = './trials/csv/trial1eeg.csv'
-marker_filerpath = './trials/csv/trial1marker.csv'
+marker_filepath = './trials/csv/trial1marker.csv'
 broadcast_marker = 'wait'; ##Marker sent when first broadcasting the marker stream
+FREQUENCIES = [7.5, 10]
+tmin = 0;
+tmax = 100; 
+fmin = 3;
+fmax= 65;
+
+###Dev variables 
+plot = True
+save_eeg_plot = False
+eeg_7_5Hz_fp = './plots/eeg_plots/7_5Hz/'
+eeg_10Hz_fp = './plots/eeg_plots/10Hz/'
+
+save_psd_plot = True
+psd_fp = './plots/psd_plots/'
 
 ###Create the info object for our EEG raw data
-n_channels = 32
-sampling_freq = 500.0
-ch_names = [ "Fp1","Fp2","AF3","AF4","F7","F3","Fz","F4","F8","FT7","FC3","FCz","FC4","FT8","T7","C3","Cz","C4","T8","TP7","CP3","CPz","CP4","TP8","P7","P3","Pz","P4","P8","O1","Oz","O2"]
-ch_types = ["eeg"] * 32
-info = create_info(ch_names, ch_types=ch_types, sfreq=sampling_freq)
-info.set_montage("standard_1020")
+info = utils.createInfoObject(name = 'Cygnus')
 occ_all = ["P3","P7","Pz","P4","P8","O1","Oz","O2"]
-Pp = ["P3","P7","Pz","P4","P8"]
-Occ = ["O1","Oz","O2"]
 print(info)
 
-###Create Raw Array and visualize chosen channels
+###Create mne Raw Array
 eeg_df = pd.read_csv(eeg_filepath)
 print(eeg_df.head())
-
 eeg_data = np.array(eeg_df.drop("Time_stamp", axis = 1, inplace=False).T)
-print(eeg_data)
-
 eeg_raw = RawArray(eeg_data, info)
 
-eeg_raw.set_eeg_reference(["Cz"], projection=False, verbose = False)
+###Notch and FIR low-pass filter
+eeg_raw.set_eeg_reference('average', projection=False, verbose = False)
 eeg_raw.filter(0.1,None, fir_design = "firwin")
 eeg_raw.notch_filter(freqs = (60,120))
 
-###Adding events to the raw object - TODO: Create annotations object to plot markers
-markers = pd.read_csv(marker_filerpath) 
-markers = markers[markers['Marker'] != broadcast_marker]
-markers = markers.reset_index(drop = True)
-markers['Time_stamp'] = np.round(markers['Time_stamp'] - markers['Time_stamp'] [0] + paradigm_onset_delay).astype(float)
-print(markers.head())
+###Adding events to the raw object
+markers = utils.createMarkerDF(marker_filepath, broadcast_marker, paradigm_onset_delay)
 
-###Plot results 
-eeg_raw.plot(show_scrollbars=True, show_scalebars=True, picks = Occ, block = True, scalings = 'auto')
+###Create annotations object from out marker dataframe
+annot = utils.createAnnotationObject(markers, end_stim = 'end_s', end_exp = 'end')
+eeg_raw.set_annotations(annot)
 
-eeg_raw.compute_psd(method = 'welch', fmin=3, fmax = 100, tmin=7, tmax=14, picks = occ_all).plot( average = True)
-plt.show()
-                
+###Plot results
+target_frequency = 7.5
+picks = occ_all
+
+
+###TODO: All plotting functionality should be imported from utilities.utilities
+if plot:
+    scalings = dict(eeg = 3000e-1)
+    fig = None
+    ax = None
+    tf_fp = None
+
+    ###Next plot PSD for each channel in target channel and average PSD
+    if target_frequency in FREQUENCIES:
+        for tf in FREQUENCIES:
+            if tf == 7.5:
+                tmin = 7
+                tmax = 15
+                tf_fp = "7_5Hz/"
+
+                ###Plot EEG data for 7.5 Hz
+                fig = eeg_raw.plot(start = tmin - 2, duration = 12, show_scalebars=True, picks = occ_all, block = True, scalings = scalings)
+                fig.savefig(eeg_7_5Hz_fp +'7_5Hz_eeg.png') if save_eeg_plot else None
+            
+            elif tf == 10:
+                tmin = 24 
+                tmax = 33
+                tf_fp = "10Hz/"
+
+                ###Plot EEG data for 10 Hz
+                fig = eeg_raw.plot(start = tmin - 4, duration = 14, show_scalebars=True, picks = occ_all, block = True, scalings = scalings)
+                fig.savefig(eeg_10Hz_fp + '10Hz_eeg.png') if save_eeg_plot else None
+
+        
+            ###Plot PSD for each individual target channel
+            for ch in picks:
+                spect = eeg_raw.compute_psd(method = 'welch', fmin=fmin, fmax = fmax, tmin=tmin ,tmax=tmax, picks = ch)
+                _, ax = plt.subplots()
+                fig= spect.plot(axes = ax, color = utils.getOP_colors(ch), spatial_colors = False)
+                ax.set_title("PSD of " + str(ch) + " channel at " + str(tf) + "Hz target frequency")
+                ax.axvline(x = tf, ymin = 0, ymax=1, linestyle = '--', color = utils.getOP_colors(ch))
+                fig.savefig(psd_fp+tf_fp+str(ch)+'.png', dpi = 1000) if save_psd_plot else None
+                plt.show()
+
+            ###Plot average PSD
+            if tf == 7.5:
+                fig = eeg_raw.compute_psd(method = 'welch', fmin=fmin, fmax = fmax, tmin=tmin ,tmax=tmax, picks = picks).plot(average = True)
+                fig.savefig(psd_fp+tf_fp+'psd_av_7_5Hz.png') if save_psd_plot else None
+                plt.show()
+            elif tf == 10:
+                fig = eeg_raw.compute_psd(method = 'welch', fmin=fmin, fmax = fmax, tmin=tmin ,tmax=tmax, picks = picks).plot(average = True)
+                fig.savefig(psd_fp+tf_fp+'psd_av_10Hz.png') if save_psd_plot else None
+                plt.show()
--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,11 +1,16 @@
+import pandas as pd 
 import numpy as np
 import csv 
+from mne import create_info, Annotations
+import matplotlib.colors as pcolors
 
+######## Stimulus paradigm
 ##Convert ms to frames 
 def convertMStoFrames(refresh_rate, time_ms):
     delta = 1000/refresh_rate
     return np.round(time_ms/delta).astype(int)
 
+######## File I/0
 ##Write CSV file from list of tuples 
 def write_tuple_to_CSV(filepath, data_tup_list, header):
     with open(filepath,'w', newline='' ) as file:
@@ -20,3 +25,64 @@ def write_rows_to_CSV(filepath, data_tup_list, header):
         writer = csv.writer(file)
         writer.writerow(header)
         writer.writerows(data_tup_list)
+
+
+######## Data analysis
+###Create mne info object based on the desire deeg device
+def createInfoObject(name, types = 'eeg'):
+    print("Now creating mne info object for " + str(name) + str(types) + " device ")
+    
+    ### For now leave these hard-coded info parameters for the Cygnus device. 
+    ### In the future, we might want to read from a file to set them given another device 
+    n_channels = 32
+    sampling_freq = 500.0
+    ch_names = [ "Fp1","Fp2","AF3","AF4","F7","F3","Fz","F4","F8","FT7","FC3","FCz","FC4","FT8","T7","C3","Cz","C4","T8","TP7","CP3","CPz","CP4","TP8","P7","P3","Pz","P4","P8","O1","Oz","O2"]
+    ch_types = ["eeg"] * n_channels
+    info = create_info(ch_names, ch_types=ch_types, sfreq=sampling_freq)
+    info.set_montage("standard_1020")
+    
+    return info     
+
+def createMarkerDF(marker_filerpath, broadcast_marker, paradigm_onset_delay):
+    df = pd.read_csv(marker_filerpath) 
+    df = df[df['Marker'] != broadcast_marker]
+    df = df.reset_index(drop = True)
+    df['Time_stamp'] = np.round(df['Time_stamp'] - df['Time_stamp'] [0] + paradigm_onset_delay).astype(float)
+
+    return df
+
+def createAnnotationObject(marker_df, end_stim, end_exp):
+    markers = list(marker_df['Marker'])
+    ts = list(marker_df['Time_stamp'])
+    onset = []
+    description = []
+    duration = []
+
+    for idx in range(len(markers)):
+        if markers[idx]!= end_stim and markers[idx] != end_exp:
+            onset.append(ts[idx])
+            description.append(markers[idx])
+
+        elif markers[idx] == end_stim:
+            duration.append(ts[idx]- ts[idx-1])
+
+    return Annotations(onset = onset, duration = duration, description = description)
+
+######## Plotting utilities
+
+###Nice colors for our occipital and parietal eeg channels
+def getOP_colors(ch):
+    css = pcolors.CSS4_COLORS
+    
+    colordic = {"P7":"darkgreen", 
+                "P3":"rebeccapurple",
+                "Pz":"teal",
+                "P4":"maroon",
+                "P8":"navy",
+                "O1":"darkorange",
+                "Oz":"crimson",
+                "O2":"deeppink"}
+    
+    return css[colordic[ch]]
+
+
